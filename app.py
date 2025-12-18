9fb43d78088550366de0d52d994b2ddea4bc129d
Jasper Kooiman <jasper.kooiman@gmail.com>
2025-12-18 17:17:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
+from flask import Flask, render_template, session, redirect
+import json, random, qrcode, io, base64
+
+app = Flask(__name__)
+app.secret_key = "hitster-kids-secret"  # nodig voor session
+
+with open("songs.json", "r") as f:
+    SONGS = json.load(f)
+
+def generate_qr(url):
+    qr = qrcode.make(url)
+    buf = io.BytesIO()
+    qr.save(buf)
+    buf.seek(0)
+    return base64.b64encode(buf.getvalue()).decode()
+
+@app.route("/")
+def index():
+    song = random.choice(SONGS)
+    session["song"] = song
+
+    qr_base64 = generate_qr(song["spotify_track_url"])
+    return render_template(
+        "index.html",
+        qr=qr_base64,
+        song=None
+    )
+
+@app.route("/reveal")
+def reveal():
+    song = session.get("song")
+    qr_base64 = generate_qr(song["spotify_track_url"])
+    return render_template(
+        "index.html",
+        qr=qr_base64,
+        song=song
+    )
+
+@app.route("/next")
+def next_song():
+    return redirect("/")
 
